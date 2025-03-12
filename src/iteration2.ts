b518cff5f46b5c5c3bb879c61db6003ee9cef5a9
ArturGR3 <arturgrygorian3@gmail.com>
2025-03-12 13:14:25 +0100

    done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,17 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication(a: number, b: number): number {
+  return a * b;
+}
 
+function isEven(n: number): boolean {
+  return n % 2 === 0;
+}
+
+function calcArrayAverage(numbersArr: Array<number>): string | number {
+  if (!numbersArr.length) {
+    return "Please provide an array with at least one element";
+  }
+  return numbersArr.reduce((acc, current) => acc + current, 0) / numbersArr.length;
+}
